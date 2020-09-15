cad4e8d4fff6988fd26cf808a5ed5cc119dd4ea3
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-15 19:01:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,11 @@ def line(deli)
     puts "The line is currently empty."
   else
     counter = 1
-    puts "The line is currently: "
+    message = "The line is currently: "
     deli.each do |customer| 
-      puts "#{counter}. #{customer}"
+      message += "#{counter}. #{customer}"
       counter += 1
     end
+    puts message
   end
 end
