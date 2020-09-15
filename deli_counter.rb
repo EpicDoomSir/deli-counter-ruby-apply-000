c6722f37c89956e1e74fa79ed5a41b2d6f3ea24a
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-15 19:00:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,12 @@ katz_deli = []
 def line(deli)
   if deli.count == 0
     puts "The line is currently empty."
+  else
+    counter = 1
+    puts "The line is currently: "
+    deli.each do |customer| 
+      puts "#{counter}. #{customer}"
+      counter += 1
+    end
   end
 end
